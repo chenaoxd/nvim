d003852ab525819d9fb359d52acf3dfe40f5c207
Ao Chen <chenao3220@gmail.com>
2024-04-15 01:20:57 +0800

    Improve configs

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = LazyVim.safe_keymap_set
+
+map("n", "<leader>ss", ":mksession! ~/.vimsession<cr>", { desc = "Save session" })
+map("n", "<leader>sl", ":source ~/.vimsession<cr>", { desc = "Load session" })
